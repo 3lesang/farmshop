69a21779c586f4cd37d340ad21dad6e4ec707e08
Khang <19110374@student.hcmute.edu.vn>
2021-12-02 22:57:31 +0700

    Update Aside.js

--- frontend/v2/src/components/Aside.js
+++ frontend/v2/src/components/Aside.js
@@ -1,26 +1,36 @@
+import { parseRequestUrl } from '../utils.js';
+import { getCategories } from '../api/category.js';
 const Aside = {
 	render: async () => {
-		return `
-   <div class="aside-header">
-    <div>SHOP BY CATEGORY</div>
-    <button class="aside-close-button" id="aside-close-button">&#x274C;</button>
-  </div>
-  <div class="aside-body">
-    <ul class="categories">
-      <li>
-        <a href="/#/?q=nho"
-          >Nho
-          <span><i class="fa fa-chevron-right"></i></span>
-        </a>
-      </li>
-      <li>
-        <a href="/#/?q=xoai"
-          >Xoai
-          <span><i class="fa fa-chevron-right"></i></span>
-        </a>
-      </li> 
-    </ul>
-  </div>`;
+
+    const { value } = parseRequestUrl();
+    const categories = await getCategories({ searchKeyword: value });
+    if (categories.error) {
+      return `<div class="error">${categories.error}</div>`;
+    }
+    console.log(categories);
+
+
+    
+    return `
+      <div class="aside-header">
+        <div>SHOP BY CATEGORY</div>
+          <button class="aside-close-button" id="aside-close-button">&#x274C;</button>
+      </div>
+      <div class="aside-body">
+        <ul class="categories">
+          ${categories
+            .map(
+              (category) => `
+                <a href="/#/?q=${category.name}"
+                  >${category.name}
+                  <span><i class="fa fa-chevron-right"></i></span>
+                </a>
+            `
+            )
+            .join('\n')}   
+        </ul>
+      </div>`;
 	},
 	after_render: async () => {
 		document.getElementById('aside-container').classList.remove('open');
