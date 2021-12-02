b7650d5f34ba064595d2587dec21c3d1998d8b7a
Khang <19110374@student.hcmute.edu.vn>
2021-12-02 22:57:22 +0700

    Update index.php

--- frontend/v2/index.php
+++ frontend/v2/index.php
@@ -10,8 +10,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chartist/0.11.4/chartist.min.css" integrity="sha512-V0+DPzYyLzIiMiWCg3nNdY+NyIiK9bED/T1xNBj08CaIUyK3sXRpB26OUCIzujMevxY9TRJFHQIxTwgzb0jVLg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
+    <link
+        href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        rel="stylesheet" type="text/css">
     <title>FarmShop</title>
-</head>
+</head> 
 
 <body>
     <div class="overlay" id="loading-overlay">Loading...</div>
@@ -19,11 +22,8 @@
     <div class="grid-container" id="page">
         <header id="header-container"></header>
         <aside id="aside-container"></aside>
-        <main id="main-container">
-        </main>
-        <footer>
-            All rights reserved @2021
-        </footer>
+        <main id="main-container"></main>
+        <footer id="footer-container"></footer>
     </div>
     <script type="module" src="src/index.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/chartist/0.11.4/chartist.min.js" integrity="sha512-9rxMbTkN9JcgG5euudGbdIbhFZ7KGyAuVomdQDI9qXfPply9BJh0iqA7E/moLCatH2JD4xBGHwV6ezBkCpnjRQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
